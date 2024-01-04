638e4e436da9aee44b4060cb6bdf263060b46c0a
luisalbeto <luis-alberto11@hotmail.com>
2024-01-04 16:52:17 -0400

    Orders Entity (Admin)

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -31,7 +31,7 @@ export const ProductClient: React.FC<ProductClientProps> = ({
         </Button>
       </div>
       <Separator/>
-      <DataTable searchKey="label" columns={columns} data={data}/>
+      <DataTable searchKey="name" columns={columns} data={data}/>
       <Heading title="API" description="API calls for products"/> 
       <Separator/>
       <ApiList entityName="products" entityIdName="productId"/>
